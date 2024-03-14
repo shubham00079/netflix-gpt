7b2a99b1b6c0b029d9120c5986319a16685e50e1
shubham-kashyap-lng <142972741+shubham-kashyap-lng@users.noreply.github.com>
2024-03-14 14:40:10 +0530

    sign in/up form && Login/Header

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import './App.css';
+import Body from "./components/Body";
 
 function App() {
   return (
-    <div className='text-3xl font-bold text-red-500'>
-      Namaste Everyone Let's build Netflix GPT
-    </div>
+    <Body/>
   );
 }
 
